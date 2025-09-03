894c16f9f447f76b793faae8d7582b115034e93d
Suhit17 <suhit1.gupta@gmail.com>
2025-09-03 14:05:29 +0530

    Delete Desktop/Build w_AI Advanced/new_stock_analysis directory
    wrong place

--- Desktop/Build w_AI Advanced/new_stock_analysis/test_streamlit.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("🧪 Test Streamlit App")
-st.write("If you can see this, Streamlit is working!")
-st.success("Connection successful! ✅")
-
-if st.button("Click me!"):
-    st.balloons()
-    st.write("Button clicked! The app is responsive.")
